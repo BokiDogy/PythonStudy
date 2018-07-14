26df46ad3840ceb052716d20c3aa189d4dc86d1b
BokiDogy <wbocai@gmail.com>
2018-07-14 16:46:47 +0800

    0714 start srudy python

--- Hello World.py
+++ Hello World.py
@@ -1,2 +1,7 @@
 print 'Hello World!'
 print 'Hello Python !'
+print  "I like typint this."
+print "This is fun."
+print "Yay!Pringting."
+print "I'd much rather you 'not'"
+print 'I "said" do not touch this.'
